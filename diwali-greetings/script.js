dc27ad442860144af504cd0909f0654195b3650d
Rohit Nandi <rohitnandibmp@gmail.com>
2024-09-07 04:34:14 +0530

    ..

--- diwali-greetings/script.js
+++ diwali-greetings/script.js
@@ -9,8 +9,40 @@ function debounce(func, delay) {
   };
 }
 
-let btn = document.querySelector("button");
-let debouncedClick = debounce(() => {
-  console.log("Clicked");
-}, 500);
-btn.addEventListener("click", debouncedClick);
+Array.prototype.myMap = function (func, args) {
+  let arrayPassed = this;
+  if (arrayPassed == null) {
+    throw new TypeError("Method is called for null or undefined");
+  }
+  if (typeof func !== "function") {
+    throw new TypeError("Invalid callback passed");
+  }
+
+  let result = new Array(arrayPassed.length);
+  for (let i = 0; i < arrayPassed.length; i++) {
+    result[i] = func.call(args, arrayPassed[i], i, Object(this));
+  }
+  return result;
+};
+
+Array.prototype.myFilter = function (func, args) {
+  let arrayPassed = this;
+
+  let result = [];
+  for (let i = 0; i < arrayPassed.length; i++) {
+    if (func.call(args, arrayPassed[i], i, Object(this))) {
+      result.push(arrayPassed[i]);
+    }
+  }
+
+  return result;
+};
+
+Array.prototype.myReduce = function (func) {};
+
+let arr = [1, 2, 3];
+let newArr = arr.myFilter((item, i) => {
+  return item >= 2;
+});
+
+console.log(newArr);
